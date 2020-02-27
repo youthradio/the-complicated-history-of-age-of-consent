8b6c82aa3278280ff2e7b6bc877dc3d4fb2f4dc7
Radames Ajna <radamajna@gmail.com>
2020-02-27 15:19:58 -0800

    SEO config

--- post.config.js
+++ post.config.js
@@ -1,19 +1,19 @@
 export default {
-  baseURL: '/history-of-age-consent',
+  baseURL: '/the-complicated-history-of-age-of-consent',
   title: 'Illegal Or Just Creepy? The Complicated History of Age of Consent',
   author: 'Jen Tribbet',
   publishDate: 'Jan. 28, 2020',
   location: 'Oakland, CA',
-  description: 'SEO friendly Description',
+  description: 'An interactive report on the history and cultural politics of "age of consent" laws and what they reveal about the boundary between relationships and crimes.',
   url: 'CANONICAL URL',
   featureImage:
-    'https://interactive.yr.media/college-app-guide/images/college-guide-featurex1875.jpg',
+    'https://interactive.yr.media/the-complicated-history-of-age-of-consent/iamges/age_of_consent.jpg',
   featureImagePath: 'images/aoc-history',
   featureImageDescription: 'Woman looking at kiosk.',
   featureImageCaption: null,
 
-  wpPostSlug: 'north-carolina-vs-vaping-companies',
-  wpPostID: '60986',
+  wpPostSlug: null,
+  wpPostID: null,
   // you might not need to change
   fbAppID: '73080818131',
   twitterHandler: '@itsyrmedia'
